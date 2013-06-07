f151034bb1f3cf6255f6db8925c6fbd10c4b0b45
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2013-06-07 16:13:08 -0400

    Added bootstrap data and added commented H2 db config for those not wanting to set up a postresql db.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,15 @@
-import grails.converters.JSON
-import org.arbfile.json.BookMarshaller
+import org.arbfile.Book
 
 class BootStrap
 {
 
     def init = { servletContext ->
+        Book book = new Book(title:"Rapid Development", author:"Steve McConnell", releaseDate:new Date().parse("MM/dd/yyyy", "05/01/2004"), keywords: "code practic", coverImage:"/grails-backbone/static/images/book.png")
+        book.save()
+        book = new Book(title:"Domain Driven Design", author:"Evans", releaseDate:new Date().parse("MM/dd/yyyy", "05/01/2010"), keywords: "domain", coverImage:"/grails-backbone/static/images/book.png")
+        book.save()
+        book = new Book(title:"Refactoring", author:"Martin Fowler", releaseDate:new Date().parse("MM/dd/yyyy", "05/01/2007"), keywords: "refactoring java", coverImage:"/grails-backbone/static/images/book.png")
+        book.save()
     }
     def destroy = {
     }
